3b03d67cbe845f66ee2554bc6f64a15594090d0b
TramVo276 <tvo941352@gmail.com>
2024-09-12 10:16:35 -0700

    haha

--- Problem2.java
+++ Problem2.java
@@ -5,10 +5,10 @@ public class Problem2
         double x = 57.3934;
         double y = 22.5211;
         System.out.println("Two original numbers:");
-        System.out.println(x);
-        System.out.println(y);
-        double rounded = int(y + 0.5);
-        double rounded2 = int(x + 0.5);
+        System.out.println (x);
+        System.out.println (y);
+        double rounded = int (y + 0.5);
+        double rounded2 = int (x + 0.5);
         System.out.println(rounded + rounded2);
 
 
