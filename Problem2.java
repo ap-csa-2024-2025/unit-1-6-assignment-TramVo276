db0b191ba8a68fa87ed035392064270815e21c7b
TramVo276 <tvo941352@gmai.com>
2024-09-12 10:14:58 -0700

    lili

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,16 @@
+public class Problem2
+{
+    public static void main(String[]args)
+    {
+        double x = 57.3934;
+        double y = 22.5211;
+        System.out.println("Two original numbers:");
+        System.out.println(x);
+        System.out.println(y);
+        double rounded = int(y + 0.5);
+        double rounded2 = int(x + 0.5);
+        System.out.println(rounded + rounded2);
 
+
+    }
+}
