7c51f4e84a7d79201707f52c60667e07155f32f3
TramVo276 <tvo941352@gmai.com>
2024-09-12 10:07:58 -0700

    lala

--- Problem1.java
+++ Problem1.java
@@ -2,7 +2,7 @@ public class Problem1
 {
   public static void main(String[] args)
   {
-    double num = 6 / 20;
-    System.out.print(num);
+    double x = 5.0 / 20.0;
+    System.out.print(x);
   }
 }
